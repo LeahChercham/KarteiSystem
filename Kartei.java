e260748098336d1d086b731874fc5742c160f2d2
Leah Chercham <leahchercham@hotmail.com>
2020-01-19 20:37:46 +0100

    sachen eingeben geht

---
+++ Kartei.java
@@ -0,0 +1,47 @@
+/*
+Die Klasse Kartei verwaltet die gesamten Freunde
+Version 1.0
+Author: Leah Chercham
+*/
+import java.util.Scanner;
+public class Kartei {
+    private static int bestand = 0;
+
+    // Referenzvariable refFreund von Typ Freund anlegen und
+    // mit null initialisieren
+    
+    // Eingabe rufen
+    public static void main(String[] args)
+    {
+        Freund refFreund = null;
+        Scanner eingabe = new Scanner(System.in);
+
+        System.out.print("Vorname eingeben: ");
+        String vorname = eingabe.next();
+
+        System.out.print("Nachname eingeben: ");
+        String nachname = eingabe.next();
+        System.out.print("Geburtstag eingeben: ");
+        String geburtstag = eingabe.next();
+        System.out.print("Handy Nummer eingeben: ");
+        int handy = eingabe.nextInt();
+        System.out.print("Telefon Nummer eingeben: ");
+        int telefon = eingabe.nextInt();
+        System.out.print("Adresse eingeben: ");
+        String adresse = eingabe.next();
+        System.out.print("Schluessel eingeben: ");
+        String schluessel = eingabe.next();
+
+        refFreund = new Freund(vorname, nachname, geburtstag, telefon, handy, adresse, schluessel);
+        bestand++;
+        System.out.println("Der Freund " + vorname + " " + nachname + " wurde in der Kartei angelegt.");
+        System.out.println("Du hast " + bestand + " Freunde in deiner Kartei.");
+        eingabe.close();
+    }
+
+    
+    // Eingabe speichern new Freund eingabe
+
+    //
+
+}
