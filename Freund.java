cdec4a609adcfb2b3e5fa250c98610766deed34a
Leah Chercham <leahchercham@hotmail.com>
2020-07-12 15:52:57 +0200

    comments freund class

--- Freund.java
+++ Freund.java
@@ -7,16 +7,12 @@ Author: Leah Chercham
 */
 
 public class Freund implements Serializable {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
     private String vorname;
     private String nachname;
     private String geburtstag;
     private int telefon;
     private int handy;
-    private String adresse; // Als Klasse ? Braucht PLZ Ort und Str.
+    private String adresse;
     private String schluessel; // eindeutige identifizierung mit stringified UUID
 
     public Freund(String vorname, String nachname, String geburtstag, int telefon, int handy, String adresse,
@@ -30,92 +26,106 @@ public class Freund implements Serializable {
         this.schluessel = schluessel;
     }
 
-    public void modifyFriend(String attribute, String text, int nummern) {
-        // Switch statement for attribute, pass value
-        switch (attribute) {
-            case "vorname": {
-                this.setVorname(text);
-                break;
-            }
-            case "nachname": {
-                this.setNachname(text);
-                break;
-            }
-            case "geburtstag": {
-                this.setGeburtstag(text);
-                break;
-            }
-            case "telefon": {
-                this.setTelefon(nummern);
-                break;
-            }
-            case "handy": {
-                this.setHandy(nummern);
-                break;
-            }
-            case "adresse": {
-                this.setAdresse(text);
-                break;
-            }
-        }
-    }
 
+    // Die Set-Methoden nehmen Veränderungen an der Instanz der Klasse vor.
 
-    // ********* Set methods
+    /** Die Methode setNachname() veraendert den Nachnamen des Freundes.
+     * @param nachname Nachname des Freundes.
+     */
     public void setNachname(String nachname) {
         this.nachname = nachname;
     }
 
+    /** Die Methode setVorname() veraendert den Vornamen des Freundes.
+     * @param vorname Vorname des Freundes.
+     */
     public void setVorname(String vorname) {
         this.vorname = vorname;
     }
 
+    /** Die Methode setGeburtstag() veraendert den Geburtstag des Freundes.
+     * @param geburtstag Geburtstag des Freundes.
+     */
     public void setGeburtstag(String geburtstag) {
         this.geburtstag = geburtstag;
     }
 
+    /** Die Methode setTelefon() veraendert die Telefonnummer des Freundes.
+     * @param telefon Telefon des Freundes.
+     */
     public void setTelefon(int telefon) {
         this.telefon = telefon;
     }
 
+    /** Die Methode setHandy() veraendert die Handynummer des Freundes.
+     * @param handy Handy des Freundes.
+     */
     public void setHandy(int handy) {
         this.handy = handy;
     }
 
+    /** Die Methode setAdresse() veraendert die Adresse des Freundes.
+     * @param adresse Adresse des Freundes.
+     */
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
 
 
-    // ********* Get methods
+    // Die Set-Methoden nehmen Veränderungen an der Instanz der Klasse vor.
+
+    /** Die Methode getName() gibt den vollstaendigen Namen des Freundes zurück.
+     * @return vollstaendiger Name der Person von Typ String.
+     */
     public String getName() {
         return (vorname + " " + nachname);
     }
 
+    /** Die Methode getVorname() gibt den Vornamen des Freundes zurück.
+     * @return vorname der Person von Typ String.
+     */
     public String getVorname() {
         return vorname;
     }
 
+    /** Die Methode getNachname() gibt den Nachnamen des Freundes zurück.
+     * @return nachname der Person von Typ String.
+     */
     public String getNachname() {
         return nachname;
     }
 
+    /** Die Methode getGeburtstag() gibt den Geburtstag des Freundes zurück.
+     * @return geburtstag der Person von Typ String.
+     */
     public String getGeburtstag() {
         return geburtstag;
     }
 
+    /** Die Methode getHandy() gibt die Handynummer des Freundes zurück.
+     * @return handy der Person von Typ int.
+     */
     public int getHandy() {
         return handy;
     }
 
+    /** Die Methode getTelefon() gibt die Telefonnummer des Freundes zurück.
+     * @return telefon der Person von Typ int.
+     */
     public int getTelefon() {
         return telefon;
     }
 
+    /** Die Methode getAdresse() gibt die Adresse des Freundes zurück.
+     * @return Adresse der Person von Typ String.
+     */
     public String getAdresse() {
         return adresse;
     }
 
+    /** Die Methode getSchluessel() gibt die Schluessel des Freundes zurück.
+     * @return Schluessel der Person von Typ String.
+     */
     public String getSchluessel() {
         return schluessel;
     }
